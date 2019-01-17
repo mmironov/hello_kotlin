4b115a143c76ccdcb76e53b537f2edcfb31dcccf
Miroslav <miroslavmironov@gmail.com>
2019-01-17 17:57:46 +0200

    Initial commit

---
+++ src/com/miro/helloworld/HelloWorld.kt
@@ -0,0 +1,5 @@
+package com.miro.helloworld
+
+fun main(args: Array<String>) {
+
+}
---
+++ src/com/miro/helloworld/Student.java
@@ -0,0 +1,15 @@
+package com.miro.helloworld;
+
+public class Student {
+
+    String name;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void add(int[] array) {
+
+    }
+}
